590db6858fd6777e9bfb8e8fdd729805817f8cf6
abelbcarvalho <abelbdecarvalho@gmail.com>
2020-07-28 13:30:18 -0300

    Login Model Class is Ready

--- src/com/bcarlog/login/model/Login.java
+++ src/com/bcarlog/login/model/Login.java
@@ -12,12 +12,54 @@ import com.bcarlog.struct.model.AbsModel;
  * @author abelbcarvalho
  */
 public class Login extends AbsModel {
+    private String siteLogin;
+    private String userLogin;
+    private String emailLogin;
+    private String passLogin;
 
     /**
      * Create a new Login Instance.
      */
     public Login() {
         super();
+        this.siteLogin = "";
+        this.userLogin = this.siteLogin;
+        this.emailLogin = this.userLogin;
+        this.passLogin = this.emailLogin;
+    }
+
+    // getters and setters
+    
+    public String getSiteLogin() {
+        return siteLogin;
+    }
+
+    public void setSiteLogin(String siteLogin) {
+        this.siteLogin = siteLogin;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public String getEmailLogin() {
+        return emailLogin;
+    }
+
+    public void setEmailLogin(String emailLogin) {
+        this.emailLogin = emailLogin;
+    }
+
+    public String getPassLogin() {
+        return passLogin;
+    }
+
+    public void setPassLogin(String passLogin) {
+        this.passLogin = passLogin;
     }
     
 }
