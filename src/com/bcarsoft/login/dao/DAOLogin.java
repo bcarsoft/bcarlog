a6475616593afdb4cc6aa2f5d4a9f044e0ee33dd
abelbcarvalho <abelbdecarvalho@gmail.com>
2020-08-14 16:27:38 -0300

    Login DAO is ready

--- src/com/bcarsoft/login/dao/DAOLogin.java
+++ src/com/bcarsoft/login/dao/DAOLogin.java
@@ -7,6 +7,9 @@ package com.bcarsoft.login.dao;
 
 import com.bcarsoft.login.model.Login;
 import com.bcarsoft.struct.dao.DAOBase;
+import com.bcarsoft.struct.sql.ConFactory;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,12 +17,14 @@ import java.util.List;
  * @author abelbcarvalho
  */
 public class DAOLogin extends DAOBase implements IDAOLogin {
+    private String sql;
 
     /**
      * New Instance of DAO Login Class.
      */
     public DAOLogin() {
         super();
+        this.sql = "";
     }
 
     /**
@@ -29,7 +34,17 @@ public class DAOLogin extends DAOBase implements IDAOLogin {
      */
     @Override
     public boolean saveLogin(Login login) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // checks if username or email are existent
+        if (this.thereAreUserOrEmailWithLogin(login)) return false;
+        // sql query
+        this.setSql(
+            "INSERT INTO tbLogin (siteLogin,urlSiteLogin,userLogin,emailLogin,"
+                    + "passLogin,idAcc) VALUES (?,?,?,?,?,?)"
+        );
+        // insert data into a List
+        this.insertFromObjectInsideList(login);
+        this.getData().add(login.getFk());
+        return this.save(this.getSql());
     }
 
     /**
@@ -39,7 +54,18 @@ public class DAOLogin extends DAOBase implements IDAOLogin {
      */
     @Override
     public boolean updateLogin(Login login) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // checks if username or email are existent
+        if (this.thereAreUserOrEmailWithLogin(login)) return false;
+        // sql query
+        this.setSql(
+            "UPDATE tbLogin SET siteLogin=?,urlSiteLogin=?,userLogin=?,emailLogin=?,"
+                    + "passLogin=? WHERE idLogin=? AND idAcc=?"
+        );
+        // insert data into a List
+        this.insertFromObjectInsideList(login);
+        this.getData().add(login.getId());
+        this.getData().add(login.getFk());
+        return this.update(this.getSql());
     }
 
     /**
@@ -49,7 +75,8 @@ public class DAOLogin extends DAOBase implements IDAOLogin {
      */
     @Override
     public boolean deleteLogin(Login login) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.setSql("DELETE FROM tbLogin WHERE idLogin=?");
+        return this.delete(login.getId(), this.getSql());
     }
     
     /**
@@ -59,7 +86,8 @@ public class DAOLogin extends DAOBase implements IDAOLogin {
      */
     @Override
     public boolean deleteAllLogin(Login login) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.setSql("DELETE FROM tbLogin WHERE idAcc=?");
+        return this.delete(login.getId(), this.getSql());
     }
 
     /**
@@ -69,7 +97,35 @@ public class DAOLogin extends DAOBase implements IDAOLogin {
      */
     @Override
     public List<Login> findAllLogin(Login login) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // setting data and sql query
+        this.setData(new ArrayList<>());
+        this.getData().clear();
+        this.getData().add(login.getFk());
+        this.setSql(
+            "SELECT * FROM tbLogin WHERE idAcc=?"
+        );
+        // new Login list
+        List<Login> logL = new ArrayList<>();
+        // try catch
+        try {
+            this.setResult(this.findAll(this.getSql()));
+            while (this.getResult().next()) {
+                Login lg = new Login();
+                lg.setId(this.getResult().getInt("idLogin"));
+                lg.setSiteLogin(this.getResult().getString("siteLogin"));
+                lg.setUrlSiteLogin(this.getResult().getString("urlSiteLogin"));
+                lg.setUserLogin(this.getResult().getString("userLogin"));
+                lg.setEmailLogin(this.getResult().getString("emailLogin"));
+                lg.setPassLogin(this.getResult().getString("passLogin"));
+                lg.setFk(this.getResult().getInt("idAcc"));
+                logL.add(lg);
+            }
+            return logL;
+        } catch (SQLException e) {
+            return null;
+        } finally {
+            ConFactory.closeConnection(this.getCon(), this.getStmt(), this.getResult());
+        }
     }
 
     /**
@@ -80,7 +136,82 @@ public class DAOLogin extends DAOBase implements IDAOLogin {
      */
     @Override
     public List<Login> findSpecificLogin(List data, String sql) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // seting data
+        this.setData(data);
+        this.setSql(sql);
+        // new Login list
+        List<Login> logL = new ArrayList<>();
+        // try catch
+        try {
+            this.setResult(this.findAll(this.getSql()));
+            while (this.getResult().next()) {
+                Login lg = new Login();
+                lg.setId(this.getResult().getInt("idLogin"));
+                lg.setSiteLogin(this.getResult().getString("siteLogin"));
+                lg.setUrlSiteLogin(this.getResult().getString("urlSiteLogin"));
+                lg.setUserLogin(this.getResult().getString("userLogin"));
+                lg.setEmailLogin(this.getResult().getString("emailLogin"));
+                lg.setPassLogin(this.getResult().getString("passLogin"));
+                lg.setFk(this.getResult().getInt("idAcc"));
+                logL.add(lg);
+            }
+            return logL;
+        } catch (SQLException e) {
+            return null;
+        } finally {
+            ConFactory.closeConnection(this.getCon(), this.getStmt(), this.getResult());
+        }
+    }
+    
+    /**
+     * This method checks if there is email or username registered.
+     * @param login Login instance
+     * @return true if success
+     */
+    private boolean thereAreUserOrEmailWithLogin(Login login) {
+        // sql initial
+        this.setSql("SELECT idLogin FROM tbLogin WHERE (userAcc=? OR emailAcc=?) and siteLogin=?");
+        // list of parameters
+        this.setData(new ArrayList<>());
+        this.getData().clear();
+        this.getData().add(login.getUserLogin());
+        this.getData().add(login.getEmailLogin());
+        this.getData().add(login.getSiteLogin());
+        try {
+            this.setResult(this.findAll(this.getSql()));
+            this.getResult().next();
+            login.setId(this.getResult().getInt("idLogin"));
+            return true;
+        } catch (SQLException e) {
+            return false;
+        } finally {
+            ConFactory.closeConnection(this.getCon(), this.getStmt(), this.getResult());
+        }
+    }
+    
+    /**
+     * This method take the object parameters and insert into a list
+     * @param account Account instance
+     */
+    private void insertFromObjectInsideList(Login login) {
+        if (login == null) {return;}
+        this.setData(new ArrayList<>());
+        this.getData().clear();
+        this.getData().add(login.getSiteLogin());
+        this.getData().add(login.getUrlSiteLogin());
+        this.getData().add(login.getUserLogin());
+        this.getData().add(login.getEmailLogin());
+        this.getData().add(login.getPassLogin());
+    }
+    
+    // getters and setters
+
+    protected String getSql() {
+        return sql;
+    }
+
+    protected void setSql(String sql) {
+        this.sql = sql;
     }
     
 }
