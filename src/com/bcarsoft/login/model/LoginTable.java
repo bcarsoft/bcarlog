b1fd623f1b2293f101f885b7379daca9184d4bf2
abelbcarvalho <abelbdecarvalho@gmail.com>
2020-08-15 17:18:49 -0300

    Models for a table gui were added to the project, Login and Site Tables

---
+++ src/com/bcarsoft/login/model/LoginTable.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bcarsoft.login.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * Table Model for Login Center.
+ * @author abelbcarvalho
+ */
+public class LoginTable extends AbstractTableModel {
+    private final List<Login> data = new ArrayList<>();
+    private final String[] columns = {"ID","Site","Link","Usuário","E-Mail","Senha","Editar","Deletar"};
+
+    /**
+     * New Instance of LoginTable Model Class.
+     */
+    public LoginTable() {
+        super();
+    }
+
+    /**
+     * Inserts column name.
+     * @param column Integer
+     * @return String
+     */
+    @Override
+    public String getColumnName(int column) {
+        return this.columns[column];
+    }
+
+    /**
+     * Row Number.
+     * @return Integer
+     */
+    @Override
+    public int getRowCount() {
+        return this.data.size();
+    }
+
+    /**
+     * Column number.
+     * @return Integer
+     */
+    @Override
+    public int getColumnCount() {
+        return this.columns.length;
+    }
+
+    /**
+     * When updates, it is for insert column by column.
+     * @param line Integer - Line.
+     * @param col Integer - Column.
+     * @return Object that represents the cell.
+     */
+    @Override
+    public Object getValueAt(int line, int col) {
+        switch (col) {
+            case 0:
+                return data.get(line).getId();
+            case 1:
+                return data.get(line).getSiteLogin();
+            case 2:
+                return data.get(line).getUrlSiteLogin();
+            case 3:
+                return data.get(line).getEmailLogin();
+            case 4:
+                return data.get(line).getPassLogin();
+        }
+        return null;
+    }
+    
+    /**
+     * Inserts a new line.
+     * @param login Login Object
+     * @param line Line number
+     */
+    public void addRow(Login login, int line) {
+        // creating icon
+        this.data.add(login);
+        this.fireTableDataChanged(); // update table
+    }
+    
+    /**
+     * Removes a single row.
+     * @param indx Integer
+     */
+    public void removeRow(int indx) {
+        this.data.remove(indx);
+        this.fireTableRowsDeleted(indx, indx);
+    }
+    
+    /**
+     * Cleans the JTable.
+     */
+    public void removeAllRows() {
+        int end = this.getRowCount();
+        this.data.clear();
+        this.fireTableRowsDeleted(0, end);
+    }
+    
+}
---
+++ src/com/bcarsoft/site/model/SiteTable.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bcarsoft.site.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * Table Model for Site Center.
+ * @author abelbcarvalho
+ */
+public class SiteTable extends AbstractTableModel {
+    private final List<Site> data = new ArrayList<>();
+    private final String[] columns = {"ID","Nome do Site","Link do Site","Editar","Deletar"};
+
+    /**
+     * New Instance of SiteTable Model Class.
+     */
+    public SiteTable() {
+        super();
+    }
+
+    /**
+     * Inserts column name.
+     * @param column Integer
+     * @return String
+     */
+    @Override
+    public String getColumnName(int column) {
+        return this.columns[column];
+    }
+
+    /**
+     * Row Number.
+     * @return Integer
+     */
+    @Override
+    public int getRowCount() {
+        return this.data.size();
+    }
+
+    /**
+     * Column number.
+     * @return Integer
+     */
+    @Override
+    public int getColumnCount() {
+        return this.columns.length;
+    }
+
+    /**
+     * When updates, it is for insert column by column.
+     * @param line Integer - Line.
+     * @param col Integer - Column.
+     * @return Object that represents the cell.
+     */
+    @Override
+    public Object getValueAt(int line, int col) {
+        switch (col) {
+            case 0:
+                return data.get(line).getId();
+            case 1:
+                return this.data.get(line).getNameSite();
+            case 2:
+                return this.data.get(line).getUrlSite();
+        }
+        return null;
+    }
+    
+    /**
+     * Inserts a new line.
+     * @param site Site Object
+     * @param line Line number
+     */
+    public void addRow(Site site, int line) {
+        // creating icon
+        this.data.add(site);
+        this.fireTableDataChanged(); // update table
+    }
+    
+    /**
+     * Removes a single row.
+     * @param indx Integer
+     */
+    public void removeRow(int indx) {
+        this.data.remove(indx);
+        this.fireTableRowsDeleted(indx, indx);
+    }
+    
+    /**
+     * Cleans the JTable.
+     */
+    public void removeAllRows() {
+        int end = this.getRowCount();
+        this.data.clear();
+        this.fireTableRowsDeleted(0, end);
+    }
+    
+}
